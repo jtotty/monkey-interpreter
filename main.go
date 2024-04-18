b6941da91707d1d6963b0be74b2413c5d55baf3b
jtotty <jt.totty1991@gmail.com>
2024-04-19 00:28:41 +0100

    Basic start to REPL

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/jtotty/monkey-interpreter/repl"
+)
+
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
